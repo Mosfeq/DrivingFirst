5788334cf398f4a6871c904af6dcd20f60f7874f
Mosfeq <mosfeq5000@gmail.com>
2023-02-05 22:57:57 +0000

    Testing database

--- app/src/main/java/com/mosfeq/drivingfirstgithub/MainActivity.kt
+++ app/src/main/java/com/mosfeq/drivingfirstgithub/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,73 +24,17 @@ class MainActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         val uid = auth.currentUser?.uid
 
-        val learner = uid?.let {
-            db.collection("learners").document(it).get()
-                .addOnCompleteListener{ task ->
-                    if (task.isSuccessful) {
-                        val document = task.result
-                        if (document.exists()) {
-                            val role = document.getString("learner")
-                        } else {
-                            Log.d("TAG", "The document doesn't exist.")
-                        }
-                    } else {
-                        task.exception?.message?.let {
-                            Log.d("TAG", it)
-                        }
-                    }
-                }
-        }
-
-        val test = if
-
-        val instructor = uid?.let {
-            db.collection("instructors").document(it).get()
-                .addOnCompleteListener{ task ->
-                    if (task.isSuccessful) {
-                        val document = task.result
-                        if (document.exists()) {
-                            val role = document.getString("instructor")
-                        } else {
-                            Log.d("TAG", "The document doesn't exist.")
-                        }
-                    } else {
-                        task.exception?.message?.let {
-                            Log.d("TAG", it)
-                        }
-                    }
-                }
-        }
-
         et_enterEmail.text.clear()
         et_enterPassword.text.clear()
 
-        if (uid != null) {
-            db.collection("learners").document(uid)
-                .get()
-                .addOnCompleteListener{ task ->
-                    if (task.isSuccessful) {
-                        val document = task.result
-                        if (document.exists()) {
-                            val role = document.getString("learner")
-                            btn_Login.setOnClickListener {
-                                if (et_enterEmail.text.trim().isNotEmpty() && et_enterPassword.text.trim().isNotEmpty()){
-                                    signInUser(role)
-                                }else{
-                                    Toast.makeText(this,"Information required", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                        } else {
-                            Log.d("TAG", "The document doesn't exist.")
-                        }
-                    } else {
-                        task.exception?.message?.let {
-                            Log.d("TAG", it)
-                        }
-                    }
-                }
-        }
 
+        btn_Login.setOnClickListener {
+            if (et_enterEmail.text.trim().isNotEmpty() && et_enterPassword.text.trim().isNotEmpty()){
+                signInUser()
+            }else{
+                Toast.makeText(this,"Information required", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         tv_changeToRegisterPage.setOnClickListener{
             val intent = Intent(this, RegisterPage::class.java)
@@ -104,24 +47,18 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
-    private fun loginLearner(){
-        val intent = Intent(this, SearchInstructorPage::class.java)
-        startActivity(intent)
-    }
-
-    private fun loginInstructor(){
-        val intent = Intent(this, ForgotPasswordPage::class.java)
-        startActivity(intent)
-    }
-
-    private fun signInUser(string: String?){
+    private fun signInUser(){
         auth.signInWithEmailAndPassword(et_enterEmail.text.trim().toString(), et_enterPassword.text.trim().toString())
             .addOnCompleteListener{
                     task ->
                 if(task.isSuccessful){
-                    loginLearner()
-                    Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
+                    if (
+                        //use realtime database to check for user role to direct them to appropriate page
+                    ){
+                        Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, SearchInstructorPage::class.java)
+                        startActivity(intent)
+                    }
                 }else{
                     Toast.makeText(this,"Authentication Error"+task.exception,Toast.LENGTH_SHORT).show()
                 }
