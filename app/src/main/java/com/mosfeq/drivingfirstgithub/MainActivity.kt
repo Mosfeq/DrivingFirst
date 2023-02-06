29ffd0838879bed8e6b4681cd272a0cc87aef770
Mosfeq <mosfeq5000@gmail.com>
2023-02-06 23:04:51 +0000

    Trying to get role based authentication

--- app/src/main/java/com/mosfeq/drivingfirstgithub/MainActivity.kt
+++ app/src/main/java/com/mosfeq/drivingfirstgithub/MainActivity.kt
@@ -3,9 +3,9 @@ package com.mosfeq.drivingfirstgithub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.firebase.firestore.FirebaseFirestore
@@ -14,7 +14,10 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
-    private lateinit var database: FirebaseDatabase
+    private lateinit var database: DatabaseReference
+
+//    private var learner = database.child("Users").get()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +26,8 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         firestore = FirebaseFirestore.getInstance()
-        database = FirebaseDatabase.getInstance()
+        database = FirebaseDatabase.getInstance("https://driving-first-github-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Users")
         auth = FirebaseAuth.getInstance()
-        val uid = auth.currentUser?.uid
 
         et_enterEmail.text.clear()
         et_enterPassword.text.clear()
@@ -49,18 +51,41 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     private fun signInUser(){
         auth.signInWithEmailAndPassword(et_enterEmail.text.trim().toString(), et_enterPassword.text.trim().toString())
             .addOnCompleteListener{
                     task ->
                 if(task.isSuccessful){
-//                    if (
-//                        //use realtime database to check for user role to direct them to appropriate page
-//                    ){
-//                        Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
-                        val intent = Intent(this, SearchInstructorPage::class.java)
-                        startActivity(intent)
-//                    }
+                    val uid = auth.currentUser?.uid!!
+                    database.child("learner").child(uid).get().addOnSuccessListener {
+                        if (it.exists()){
+                            val role = it.child("Role").value.toString()
+                            if (role == "Learner"){
+                                Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this, SearchInstructorPage::class.java)
+                                startActivity(intent)
+                            }
+                        }
+                        else{
+                            database.child("instructor").child(uid).get().addOnSuccessListener {
+                                if (it.exists()){
+                                    val role = it.child("Role").value.toString()
+                                    if (role == "Instructor"){
+                                        Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show()
+                                        val intent = Intent(this, InstructorPage::class.java)
+                                        startActivity(intent)
+                                    }
+                                }
+                                else{
+                                    Toast.makeText(this,"Not Instructor",Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        }
+                    }
+                        .addOnFailureListener{
+                            Toast.makeText(this,"User doesn't exist",Toast.LENGTH_SHORT).show()
+                        }
                 }else{
                     Toast.makeText(this,"Authentication Error"+task.exception,Toast.LENGTH_SHORT).show()
                 }
